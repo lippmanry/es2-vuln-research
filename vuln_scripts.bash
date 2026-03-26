38c0213b8c5c84e3e9ce5000f6876f7f50186839
Ryan J Lippman <ryan@datacat-security.com>
2026-03-26 14:40:37 +0000

    more stuff to make scanners mad

--- vuln_scripts.bash
+++ vuln_scripts.bash
@@ -23,3 +23,38 @@ cat << 'EOF' > ~/vulnerable-node-app/id_rsa
 		bc6fbc6fbc6fbc6fbc6fbc6fbc6fbc6fbc6fbc6fbc6fbc6fbc6fbc6fbc6fbc6f
 		-----END RSA PRIVATE KEY-----
 EOF;
+
+cat << 'EOF' > ~/vulnerable-node-app/requirements.txt
+Django==2.2.4
+flask==0.12.1
+PyYAML==3.12
+EOF;
+
+cat << 'EOF' > ~/vulnerable-node-app/package-lock.json
+{
+    "name": "multi-vuln-app",
+    "version": "1.0.0",
+    "lockfileVersion": 2,
+    "requires": true,
+    "packages": {
+        "": {
+        "name": "multi-vuln-app",
+        "version": "1.0.0",
+        "dependencies": {
+            "axios": "0.21.1",
+            "express": "4.16.0",
+            "lodash": "4.17.20"
+        }
+        },
+        "node_modules/axios": {
+        "version": "0.21.1"
+        },
+        "node_modules/express": {
+        "version": "4.16.0"
+        },
+        "node_modules/lodash": {
+        "version": "4.17.20"
+        }
+    }
+}
+EOF;
